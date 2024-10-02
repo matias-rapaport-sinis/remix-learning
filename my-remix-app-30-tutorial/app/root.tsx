05de47c0a63368cb9f93502a82e3571ba28ef011
matias-rapa-sinis <matias.rapaport@sinis.com.ar>
2024-10-02 11:20:17 -0300

    (Adding Stylesheet with links) Que facíl practico es poder importar las hojas de estilo dentro de un objeto de Javascrip o en este caso de Typescript para mantener todo ordenado.

--- my-remix-app-30-tutorial/app/root.tsx
+++ my-remix-app-30-tutorial/app/root.tsx
@@ -6,6 +6,15 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+
+import appStylesHref from "./app.css";
+
+
+export const links : LinksFunction = () => [
+  {rel: "stylesheet", href: appStylesHref}
+]
+
 export default function App() {
   return (
     <html lang="en">
